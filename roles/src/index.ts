d1da5b7268e090c0804a7539c379cbac69f67446
EduardoVarela0144 <eduardo.varela142001@gmail.com>
2024-08-19 21:07:54 -0600

    ts config

--- roles/src/index.ts
+++ roles/src/index.ts
@@ -1,1 +1,5 @@
-console.log('Hello, world!');
+import { buildLogger } from "@plugins/logger.plugin";
+const logger = buildLogger("roles");
+
+logger.log("Hello, world!");
+logger.error("Goodbye, world!");
